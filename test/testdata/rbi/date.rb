5fab08e78bc3a8b05e6cc1e2df290131439ec57c
Trevor Elliott <awesomelyawesome@gmail.com>
2020-01-28 13:51:48 -0800

    Fix `Date#-`
    As DateTime inherits from Date, this method needs to return
    `T.self_type` instead.

--- test/testdata/rbi/date.rb
+++ test/testdata/rbi/date.rb
@@ -4,3 +4,9 @@ require "date"
 
 T.assert_type!(Date.today, Date)
 T.assert_type!(Date.today + 1, Date)
+
+T.reveal_type(Date.now - Date.now) # error: Revealed type: `Rational`
+T.reveal_type(Date.now - 10) # error: Revealed type: `Date`
+
+T.reveal_type(DateTime.now - DateTime.now) # error: Revealed type: `Rational`
+T.reveal_type(DateTime.now - 10) # error: Revealed type: `DateTime`
